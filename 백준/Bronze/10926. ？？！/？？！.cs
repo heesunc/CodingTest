8386414f040480309fa93591e71c8bc9269927e8
heesunc <112846188+heesunc@users.noreply.github.com>
2022-11-29 14:57:06 +0900

    [Bronze V] Title: ??!, Time: 60 ms, Memory: 5360 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace codingstudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Console.ReadLine() + "??!");
+        }
+    }
+}
