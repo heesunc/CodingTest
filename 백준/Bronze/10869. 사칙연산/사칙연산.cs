104ab10a18972bebd8b490256863a399824ed6ab
heesunc <112846188+heesunc@users.noreply.github.com>
2022-11-29 14:48:25 +0900

    [Bronze V] Title: 사칙연산, Time: 64 ms, Memory: 5496 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace codingstudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+
+            Console.WriteLine(int.Parse(s[0])+int.Parse(s[1]));
+            Console.WriteLine(int.Parse(s[0])-int.Parse(s[1]));
+            Console.WriteLine(int.Parse(s[0])*int.Parse(s[1]));
+            Console.WriteLine(int.Parse(s[0])/int.Parse(s[1]));
+            Console.WriteLine(int.Parse(s[0])%int.Parse(s[1]));
+        }
+    }
+}
