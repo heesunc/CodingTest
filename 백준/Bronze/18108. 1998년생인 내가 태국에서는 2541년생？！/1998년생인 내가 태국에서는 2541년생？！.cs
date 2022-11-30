64c59cb0d78a88449d28aebcc4129069ff78f631
heesunc <112846188+heesunc@users.noreply.github.com>
2022-11-30 15:53:39 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 64 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace codingstudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = int.Parse(Console.ReadLine());
+            // str로 입력되는 것을 int.Parse로 int로 바꿔줌
+            Console.WriteLine(i-543); // 출력            
+        }
+    }
+}
