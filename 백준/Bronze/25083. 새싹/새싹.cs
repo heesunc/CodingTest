81655990091874d9fa6c4d87cac68ff7e9d9b121
heesunc <112846188+heesunc@users.noreply.github.com>
2022-12-27 13:54:04 +0900

    [Bronze V] Title: 새싹, Time: 56 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace codingstudy
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("         ,r\'\"7");
+            Console.WriteLine("r`-_   ,'  ,/");
+            Console.WriteLine(" \\. \". L_r\'");
+            Console.WriteLine("   `~\\/");
+            Console.WriteLine("      |");
+            Console.WriteLine("      |");
+        }
+    }
+}
