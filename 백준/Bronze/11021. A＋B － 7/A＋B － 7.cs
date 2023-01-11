cd11ce0ea545e5ee1af7a4210fba85aa09b1821e
heesunc <112846188+heesunc@users.noreply.github.com>
2023-01-11 17:00:56 +0900

    [Bronze V] Title: A+B - 7, Time: 80 ms, Memory: 6080 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace codingstudy
+{
+    class Program
+    {
+        static void Main()
+        {
+            int t = int.Parse(Console.ReadLine()); // 테스트 케이스 입력
+            int sum = 0;
+
+            for (int i = 0; i < t; i++)
+            {
+                string[] s = Console.ReadLine().Split(); // 더할 값 입력
+                int num1 = int.Parse(s[0]);
+                int num2 = int.Parse(s[1]);
+                sum = num1 + num2;
+
+                Console.WriteLine($"Case #{i+1}: {sum}");
+            }         
+            
+        }
+    }
+}
