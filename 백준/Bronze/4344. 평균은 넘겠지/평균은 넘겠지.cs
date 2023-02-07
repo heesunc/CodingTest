7cd54c5b2b6665c0eecdd48d964919d3a1603cb5
heesunc <112846188+heesunc@users.noreply.github.com>
2023-02-07 13:36:06 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 68 ms, Memory: 6656 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.cs
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.cs
@@ -6,30 +6,24 @@ namespace codingstudy
     {
         static void Main()
         {
-            int c = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine()); // 테스트 케이스
 
             int[] scores;
             double avg;
             int cnt;
 
-            for (int i = 0; i < c; i++)
+            while (c --> 0)
             {
-                scores = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-                avg = 0;
-                cnt = 0;
-                for (int j = 1; j < scores.Length; j++)
-                {
-                    avg += scores[j];
-                }
-                avg /= scores.Length - 1;
-                for (int j = 1; j < scores.Length; j++)
-                {
-                    if (scores[j] > avg)
-                    {
-                        cnt++;
-                    }
-                }
-                Console.WriteLine("{0:F3}%", (double)cnt / (scores.Length - 1) * 100);
+                // 첫번째 요소를 제외한 나머지 문자열들을 배열로 추출
+                scores = Array.ConvertAll(Console.ReadLine().Split()[1..],int.Parse);
+                // Average() 메서드로 배열의 평균 구하기
+                avg = scores.Average();
+                // Count() 메서드로 배열의 건수 구하기
+                // 람다식으로 scores배열에서 개별 점수가 평균보다 높은 것의 개수를 구해 Count를 호출
+                // cnt에는 scores 배열에서 평균보다 높은 점수의 개수가 저장됨 
+                cnt = scores.Count(score => score > avg);
+
+                Console.WriteLine("{0:F3}%",(double)cnt / scores.Length*100);
             }
         }
     }
