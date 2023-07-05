0ef170ece6d3df63bee56573a5cb976bf3aeaa1e
heesunc <112846188+heesunc@users.noreply.github.com>
2023-07-05 15:33:57 +0900

    [unrated] Title: 코드 처리하기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.cpp
@@ -0,0 +1,47 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string code) {
+    string answer = "";
+    int mode = 0;
+    
+    for (int idx=0; idx<code.length(); idx++)
+    {
+        if (mode==0)
+        {
+            if (code[idx] != '1')
+            {
+                if (idx % 2 ==0)
+                {
+                    answer += code[idx];
+                }
+            }
+            else
+            {
+                mode = 1;
+            }
+        }
+        else if (mode==1)
+        {
+            if (code[idx] != '1')
+            {
+                if (idx % 2 !=0)
+                {
+                    answer += code[idx];
+                }
+            }
+            else
+            {
+                mode =0;
+            }
+        }
+    }
+    if (answer.empty())
+    {
+        return "EMPTY";
+    }
+    
+    return answer;
+}
