a43987ba4256ec6b15ed264c2d0e6e146ac5b278
heesunc <112846188+heesunc@users.noreply.github.com>
2023-07-14 18:03:29 +0900

    [unrated] Title: 간단한 논리 연산, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(bool x1, bool x2, bool x3, bool x4) {
+    bool answer = ( x1 + x2) * (x3 + x4);
+    
+    // ∨ 논리합 ∧ 논리곱
+    
+    return answer;
+}
