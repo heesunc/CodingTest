1029f7aa615e5aec6d1310f32a1d2e3eae86c5fa
heesunc <112846188+heesunc@users.noreply.github.com>
2023-08-05 14:17:32 +0900

    [unrated] Title: 길이에 따른 연산, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    int answer1 = 1;
+    int size = num_list.size();
+    
+    if (size >= 11)
+    {
+        for(int i=0; i<size; i++)
+            answer += num_list[i];
+            return answer;
+    }
+    else 
+    {
+        for(int j=0; j<size; j++)
+            answer1 *= num_list[j];
+            return answer1;
+    }
+}
