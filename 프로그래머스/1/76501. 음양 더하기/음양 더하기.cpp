7aad3088310060dc32c71c106603a5d4483f2873
heesunc <112846188+heesunc@users.noreply.github.com>
2023-12-01 16:36:26 +0900

    [level 1] Title: 음양 더하기, Time: 0.01 ms, Memory: 3.73 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
+    int size = absolutes.size();
+
+    for (int i = 0; i < size; i++)
+    {
+        if (signs[i] == true)
+        {
+            answer += absolutes[i];
+        }
+        else if (signs[i] != true)
+        {
+            answer -= absolutes[i];
+        }
+    }
+
+    return answer;
+}
