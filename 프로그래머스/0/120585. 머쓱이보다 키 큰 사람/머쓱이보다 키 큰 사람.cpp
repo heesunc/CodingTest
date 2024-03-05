6ccba0182a93019e508a12a68a262fbf3742304b
heesunc <112846188+heesunc@users.noreply.github.com>
2024-03-05 10:44:46 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> array, int height) {
+    int answer = 0;
+    
+    for(int i : array)
+    {
+        if (height < i)
+        {
+            answer++;
+        }
+    }
+    
+    return answer;
+}
