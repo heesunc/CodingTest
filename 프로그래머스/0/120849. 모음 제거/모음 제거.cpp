c77334421743f259288d60b1f93ac0ccbfb2a84e
heesunc <112846188+heesunc@users.noreply.github.com>
2024-03-08 12:43:41 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.cpp
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.cpp
@@ -7,8 +7,7 @@ string solution(string my_string) {
     string answer = "";
     
     for (char c : my_string) {
-        if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u' &&
-            c != 'A' && c != 'E' && c != 'I' && c != 'O' && c != 'U') {
+        if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u') {
             answer += c;
         }
     }
