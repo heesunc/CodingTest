769922b090ec035bb97c630e30a4e13e733645ed
heesunc <112846188+heesunc@users.noreply.github.com>
2024-03-11 14:15:47 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int divisor_count(int& value) {
+    int count = 0;
+    for (int i = 1; i <= value; ++i)
+        if (value % i == 0)
+            ++count;
+    return count;
+}
+
+int solution(int left, int right) {
+    int ans = 0;
+    for (int i = left; i <= right; ++i)
+        if (divisor_count(i) & 1)           // 홀수
+            ans -= i;
+        else                                // 짝수
+            ans += i;
+    return ans;
+}
