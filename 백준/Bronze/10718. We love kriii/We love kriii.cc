de7b1b02ab1b9dcae1923b25f5872a01e8f3cbd2
heesunc <112846188+heesunc@users.noreply.github.com>
2024-03-18 16:37:00 +0900

    [Bronze V] Title: We love kriii, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.cc
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <stdlib.h>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    cout << "강한친구 대한육군" << endl;
+    cout << "강한친구 대한육군" << endl;
+
+    return 0;
+}
