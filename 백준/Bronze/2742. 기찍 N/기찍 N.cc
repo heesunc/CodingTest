af93af415d14deaf7c15de81224290011aae512a
heesunc <112846188+heesunc@users.noreply.github.com>
2024-03-18 16:44:24 +0900

    [Bronze IV] Title: 기찍 N, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <stdlib.h>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n = 0;
+
+    cin >> n;
+
+    for (int i = n; i >= 1; i--)
+    {
+        cout << i << '\n';
+    }
+
+    return 0;
+}
