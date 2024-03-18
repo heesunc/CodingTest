90f4cd01810b4dc3645d74e65921142ba88b0f76
heesunc <112846188+heesunc@users.noreply.github.com>
2024-03-18 17:00:06 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.cc
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <stdlib.h>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+    cout << "고려대학교" << '\n';
+
+    return 0;
+}
