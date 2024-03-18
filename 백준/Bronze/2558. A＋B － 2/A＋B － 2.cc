456f995d18f6aa5afd215cf8aee0a99a2cd49c50
heesunc <112846188+heesunc@users.noreply.github.com>
2024-03-18 16:54:45 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <stdlib.h>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int a = 0;
+    int b = 0;
+
+    cin >> a;
+    cin >> b;
+
+    int sum = 0;
+    sum = a + b;
+
+    cout << sum << '\n';
+
+    return 0;
+}
