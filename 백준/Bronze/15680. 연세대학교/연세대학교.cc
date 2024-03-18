188dea85f253bc6f87ffaaa0d94401ec9542ace3
heesunc <112846188+heesunc@users.noreply.github.com>
2024-03-18 17:21:15 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <stdlib.h>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n = 0;
+
+    cin >> n;
+
+    if (n == 0)
+    {
+        cout << "YONSEI" << '\n';
+    }
+    else if (n == 1)
+    {
+        cout << "Leading the Way to the Future" << '\n';
+    }
+
+    return 0;
+}
