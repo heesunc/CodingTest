6738a9f6383bc0280460b758f29f33acdc57919e
heesunc <112846188+heesunc@users.noreply.github.com>
2024-03-18 16:52:37 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <stdlib.h>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    
+    cout << "       _.-;;-._" << '\n';
+    cout << "'-..-'|   ||   |" << '\n';
+    cout << "'-..-'|_.-;;-._|" << '\n';
+    cout << "'-..-'|   ||   |" << '\n';
+    cout << "'-..-'|_.-''-._|" << '\n';
+
+    return 0;
+}
