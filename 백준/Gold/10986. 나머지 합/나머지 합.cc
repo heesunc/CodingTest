ac3a718d2762576bcb474fb74a49c9af5c0d15f0
heesunc <112846188+heesunc@users.noreply.github.com>
2024-03-19 13:44:36 +0900

    [Gold III] Title: 나머지 합, Time: 136 ms, Memory: 9836 KB -BaekjoonHub

---
+++ 백준/Gold/10986. 나머지 합/나머지 합.cc
@@ -0,0 +1,48 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // 시간 단축을 위한 구문
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, M;
+    cin >> N >> M;
+
+    vector<long> S(N, 0);
+    vector<long> C(M, 0);
+
+    long answer = 0;
+    cin >> S[0];
+
+    for (int i = 1; i < N; i++)
+    {
+        int temp = 0;
+        cin >> temp;
+        S[i] = S[i - 1] + temp;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        int remainder = S[i] % M;
+        if (remainder == 0)
+        {
+            answer++;
+        }
+        C[remainder]++;
+    }
+
+    for (int i = 0; i < M; i++)
+    {
+        if (C[i] > 1)
+        {
+            answer = answer + (C[i] * (C[i] - 1) / 2);
+        }
+    }
+
+    cout << answer << "\n";
+}
