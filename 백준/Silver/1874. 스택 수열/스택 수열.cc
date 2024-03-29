63eb7fbaa32356c9ee754b5202949d47471478f6
heesunc <112846188+heesunc@users.noreply.github.com>
2024-03-29 13:55:24 +0900

    [Silver II] Title: 스택 수열, Time: 20 ms, Memory: 2944 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -0,0 +1,66 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	// 시간 단축을 위한 구문
+	ios::sync_with_stdio(false);    cin.tie(NULL);    cout.tie(NULL);
+
+	int N;
+	cin >> N;
+	vector<int> A(N, 0);
+	vector<char> resultV;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> A[i];
+	}
+
+	stack<int> myStack;
+	int num = 1;
+	bool result = true;
+
+	for (int i = 0; i < A.size(); i++)
+	{
+		int su = A[i]; // 현재 수열의 수
+		// 현재 수열값 >= 오름차순 자연수 : 값이 같아질 때까지 push() 수행
+		if (su >= num)
+		{
+			while (su >= num)
+			{
+				myStack.push(num++);
+				resultV.push_back('+');
+			}
+			myStack.pop();
+			resultV.push_back('-');
+		}
+		// 현재 수열값 < 오름차순 자연수 : pop()을 수행하여 수열 원소를 꺼냄
+		else
+		{
+			int n = myStack.top();
+			myStack.pop();
+			// 스택의 가장 위의 수가 만들어야 하는 수열의 수보다 크다면 수열 출력 불가능
+			if (n > su)
+			{
+				cout << "NO";
+				result = false;
+				break;
+			}
+			else
+			{
+				resultV.push_back('-');
+			}
+		}
+	}
+	if (result)
+	{
+		for (int i = 0; i < resultV.size(); i++)
+		{
+			cout << resultV[i] << '\n';
+		}
+	}
+}
