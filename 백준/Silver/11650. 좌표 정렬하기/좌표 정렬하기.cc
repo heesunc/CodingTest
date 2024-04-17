59f58551a3a4562ed8c5c2046bf4c86e4a35b468
heesunc <112846188+heesunc@users.noreply.github.com>
2024-04-17 12:46:26 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 44 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,40 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <list>
+#include <cctype>
+#include <stdlib.h> // stoi, itoa를 위함
+#include <cmath>
+#include <unordered_map>
+
+using namespace std;
+
+vector<pair<int, int>> v;
+
+int main() {
+	// 시간 단축을 위한 구문
+	ios::sync_with_stdio(false);    cin.tie(NULL);    cout.tie(NULL);
+
+	int N;
+	cin >> N;
+	int x, y;
+
+	for (int i = 0; i < N; i++) {
+		cin >> x >> y;
+		v.push_back({ x, y });
+	}
+
+	// pair와 같은 복합 자료구조인 경우, 
+	// 첫 번째 요소를 기준으로 먼저 정렬하고, 
+	// 첫 번째 요소가 같은 경우에는 두 번째 요소를 기준으로 정렬
+	sort(v.begin(), v.end());
+	for (int i = 0; i < N; i++) {
+		cout << v[i].first << ' ' << v[i].second << '\n';
+	}
+
+	return 0;
+}
