8b75b88630a75032966675c21690d59a6224f80f
heesunc <112846188+heesunc@users.noreply.github.com>
2024-04-18 15:41:59 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	// 시간 단축을 위한 구문
+	ios::sync_with_stdio(false);    cin.tie(NULL);    cout.tie(NULL);
+
+	int N; cin >> N;
+	
+	for (int i = N; i > 0; i--) {
+		cout << string(N - i, ' '); // 공백 출력
+		cout << string(2 * i - 1, '*') << endl; // 별표 출력
+	}
+
+	return 0;
+}
