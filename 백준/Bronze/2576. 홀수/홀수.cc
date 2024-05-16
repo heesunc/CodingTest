878948bc7bb137398be79782f67137246dcc6eb4
heesunc <112846188+heesunc@users.noreply.github.com>
2024-05-16 17:00:33 +0900

    [Bronze III] Title: 홀수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2576. 홀수/홀수.cc
@@ -0,0 +1,52 @@
+#include <string>       // 문자열 처리를 위한 헤더 파일
+#include <iostream>     // 입출력을 위한 헤더 파일
+#include <vector>       // 동적 배열(vector)을 사용하기 위한 헤더 파일
+#include <algorithm>    // 알고리즘 함수를 사용하기 위한 헤더 파일
+#include <stack>        // 스택을 사용하기 위한 헤더 파일
+#include <queue>        // 큐를 사용하기 위한 헤더 파일
+#include <deque>        // 덱(Deque)을 사용하기 위한 헤더 파일
+#include <cctype>       // 문자열의 문자를 검사하기 위한 헤더 파일
+#include <stdlib.h>     // 문자열을 숫자로 변환하는 함수를 사용하기 위한 헤더 파일
+#include <cmath>        // 수학 함수를 사용하기 위한 헤더 파일
+#include <unordered_map> // 해시맵(Unordered Map)을 사용하기 위한 헤더 파일
+#include <map>          // 맵(Map)을 사용하기 위한 헤더 파일
+#include <sstream>      // 문자열을 스트림으로 변환하기 위한 헤더 파일
+#include <cstdlib>      // 일반적인 유틸리티 함수를 사용하기 위한 헤더 파일
+#include <iomanip>      // 입출력 서식 지정을 위한 헤더 파일
+#include <climits> // INT_MAX를 사용하기 위한 헤더 파일
+
+using namespace std;  // 표준 라이브러리(std)의 네임스페이스를 사용함
+
+int main()
+{
+	ios::sync_with_stdio(false);	cin.tie(NULL);	cout.tie(NULL);
+    vector<int> A(7);
+    int sum = 0;
+    int minodd = INT_MAX;
+
+    for (int i = 0; i < 7; ++i)
+    {
+        cin >> A[i];
+    }
+
+    sort(A.begin(), A.end());
+
+    for (int i = 0; i < A.size(); i++)
+    {
+        if (A[i] % 2 != 0) // 홀수라면
+        {
+            sum += A[i];
+            minodd = min(minodd, A[i]);
+        }
+    }
+
+    if (sum == 0)
+        cout << "-1";
+    else
+    {
+        cout << sum << "\n";
+        cout << minodd;
+    }    
+
+    return 0;
+}
