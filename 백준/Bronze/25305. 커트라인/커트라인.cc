ddea0e80337e7c935df751d0506a06458516ed79
heesunc <112846188+heesunc@users.noreply.github.com>
2024-06-03 11:14:05 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/25305. 커트라인/커트라인.cc
+++ 백준/Bronze/25305. 커트라인/커트라인.cc
@@ -1,14 +1,36 @@
-#include <iostream>
-#include <algorithm>
+#include <string>       // 문자열 처리를 위한 헤더 파일
+#include <iostream>     // 입출력을 위한 헤더 파일
+#include <vector>       // 동적 배열(vector)을 사용하기 위한 헤더 파일
+#include <algorithm>    // 알고리즘 함수를 사용하기 위한 헤더 파일
+#include <stack>        // 스택을 사용하기 위한 헤더 파일
+#include <queue>        // 큐를 사용하기 위한 헤더 파일
+#include <deque>        // 덱(Deque)을 사용하기 위한 헤더 파일
+#include <cctype>       // 문자열의 문자를 검사하기 위한 헤더 파일
+#include <stdlib.h>     // 문자열을 숫자로 변환하는 함수를 사용하기 위한 헤더 파일
+#include <cmath>        // 수학 함수를 사용하기 위한 헤더 파일
+#include <unordered_map> // 해시맵(Unordered Map)을 사용하기 위한 헤더 파일
+#include <map>          // 맵(Map)을 사용하기 위한 헤더 파일
+#include <sstream>      // 문자열을 스트림으로 변환하기 위한 헤더 파일
+#include <cstdlib>      // 일반적인 유틸리티 함수를 사용하기 위한 헤더 파일
+#include <iomanip>      // 입출력 서식 지정을 위한 헤더 파일
+#include <climits> // INT_MAX를 사용하기 위한 헤더 파일
 
-using namespace std;
-
-int n, k;
-int arr[1001];
+using namespace std;  // 표준 라이브러리(std)의 네임스페이스를 사용함
 
 int main() {
-    cin >> n >> k;        
-    for(int i = 0; i < n; i++) cin >> arr[i];
-    sort(arr, arr + n);
-    cout << arr[n-k] << '\n';
+    ios::sync_with_stdio(0);    cin.tie(0);
+
+    short n, k; cin >> n >> k;
+    vector<short> A(n);
+
+    for (auto i = 0; i < n; i++)
+    {
+        cin >> A[i];
+    }
+
+    sort(A.begin(), A.end());
+
+    cout << A[n-k];
+    
+    return 0;
 }
