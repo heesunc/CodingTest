609f5531b648584079abc1b7cf8ae879b065b36c
heesunc <112846188+heesunc@users.noreply.github.com>
2024-06-05 10:19:05 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 106.59 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    arr.erase(unique(arr.begin(), arr.end()), arr.end());
+    return arr;
+}
