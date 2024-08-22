fa878279daff7bb0869a9f3e62e7044e0fdb540d
heesunc <112846188+heesunc@users.noreply.github.com>
2024-08-22 14:06:17 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -0,0 +1,50 @@
+#include <string>       // 문자열 처리를 위한 헤더 파일
+#include <iostream>     // 입출력을 위한 헤더 파일
+#include <vector>       // 동적 배열(vector)을 사용하기 위한 헤더 파일
+#include <algorithm>    // 알고리즘 함수를 사용하기 위한 헤더 파일
+#include <stack>        // 스택을 사용하기 위한 헤더 파일
+#include <queue>        // 큐를 사용하기 위한 헤더 파일
+#include <deque>        // 덱(Deque)을 사용하기 위한 헤더 파일
+#include <cctype>       // 문자열의 문자를 검사하기 위한 헤더 파일
+#include <stdlib.h>     // 문자열을 숫자로 변환하는 함수를 사용하기 위한 헤더 파일
+#include <cmath>        // 수학 함수를 사용하기 위한 헤더 파일
+#include <unordered_map> // 해시맵(Unordered Map)을 사용하기 위한 헤더 파일
+#include <map>          // 맵(Map)을 사용하기 위한 헤더 파일
+#include <sstream>      // 문자열을 스트림으로 변환하기 위한 헤더 파일
+#include <cstdlib>      // 일반적인 유틸리티 함수를 사용하기 위한 헤더 파일
+#include <iomanip>      // 입출력 서식 지정을 위한 헤더 파일
+#include <climits> // INT_MAX를 사용하기 위한 헤더 파일
+#include <numeric>  // accumulate를 사용하기 위해 필요한 헤더 파일
+
+using namespace std;  // 표준 라이브러리(std)의 네임스페이스를 사용함
+
+int main() {
+	// 시간 단축을 위한 구문
+	ios::sync_with_stdio(false);    cin.tie(NULL);    cout.tie(NULL);
+
+	int N, K; cin >> N >> K;
+	vector<int> A(N);
+
+	// 동전
+	for (int i=0; i < N; i++)
+	{
+		cin >> A[i];
+	}
+
+	int count = 0;
+	
+	// 가치가 큰 동전부터 선택해야 개수를 최소로 구성할 수 있음
+	for (int i = N-1; i >= 0; i--)
+	{
+		if (A[i] <= K) // k보다 동전 가치가 적으면
+		{
+			// 동전 수 += 목표 금액 / 현재 동전 가치
+			count += (K / A[i]);
+			// 목표 금액 = 목표 금액 % 현재 동전 가치
+			K = K % A[i];
+		}
+	}
+	cout << count;
+
+	return 0;
+}
