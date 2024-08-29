66ecd3085cb0e262f70449140f0e09fa6a80b7f6
heesunc <112846188+heesunc@users.noreply.github.com>
2024-08-29 12:34:43 +0900

    [Silver I] Title: 최대공약수, Time: 160 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1850. 최대공약수/최대공약수.cc
@@ -0,0 +1,47 @@
+#include <string>       // 문자열 처리를 위한 헤더 파일
+#include <iostream>     // 입출력을 위한 헤더 파일
+#include <vector>       // 동적 배열(vector)을 사용하기 위한 헤더 파일
+#include <algorithm>    // 알고리즘 함수를 사용하기 위한 헤더 파일
+#include <stack>        // 스택을 사용하기 위한 헤더 파일
+#include <queue>        // 큐를 사용하기 위한 헤더 파일
+#include <deque>        // 덱(Deque)을 사용하기 위한 헤더 파일
+#include <cctype>       // 문자열의 문자를 검사하기 위한 헤더 파일
+#include <stdlib.h>     // 문자열을 숫자로 변환하는 함수를 사용하기 위한 헤더 파일
+#include <cmath>        // 수학 함수를 사용하기 위한 헤더 파일
+#include <unordered_map> // 해시맵(Unordered Map)을 사용하기 위한 헤더 파일
+#include <map>          // 맵(Map)을 사용하기 위한 헤더 파일
+#include <sstream>      // 문자열을 스트림으로 변환하기 위한 헤더 파일
+#include <cstdlib>      // 일반적인 유틸리티 함수를 사용하기 위한 헤더 파일
+#include <iomanip>      // 입출력 서식 지정을 위한 헤더 파일
+#include <climits> // INT_MAX를 사용하기 위한 헤더 파일
+#include <numeric>  // accumulate를 사용하기 위해 필요한 헤더 파일
+
+using namespace std;  // 표준 라이브러리(std)의 네임스페이스를 사용함
+
+int gcd(long a, long b);
+
+int main() {
+	// 시간 단축을 위한 구문
+	ios::sync_with_stdio(false);    cin.tie(NULL);    cout.tie(NULL);
+
+	long a, b; cin >> a >> b;
+	long result = gcd(a, b);
+	while (result > 0)
+	{
+		cout << 1;
+		result--;
+	}
+	return 0;
+}
+
+int gcd(long a, long b)
+{
+	if (b == 0)
+	{
+		return a;
+	}
+	else
+	{
+		return gcd(b, a % b);
+	}
+}
