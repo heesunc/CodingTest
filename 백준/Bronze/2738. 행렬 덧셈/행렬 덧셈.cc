1fc0326f6353e4de404c81187e353ccd7a97be81
heesunc <112846188+heesunc@users.noreply.github.com>
2024-09-27 16:23:39 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 4 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,69 @@
+#include <string>       // 문자열 처리를 위한 헤더 파일
+#include <iostream>     // 입출력을 위한 헤더 파일
+#include <vector>       // 동적 배열(vector)을 사용하기 위한 헤더 파일
+#include <algorithm>    // 알고리즘 함수 (예: min())를 사용하기 위한 헤더 파일
+#include <stack>        // 스택 자료구조를 사용하기 위한 헤더 파일
+#include <queue>        // 큐 및 우선순위 큐(priority_queue)를 사용하기 위한 헤더 파일
+#include <deque>        // 덱(Deque, 양방향 큐)을 사용하기 위한 헤더 파일
+#include <cctype>       // 문자 처리를 위한 헤더 파일
+#include <stdlib.h>     // 문자열을 숫자로 변환하는 함수(예: atoi())를 위한 헤더 파일
+#include <cmath>        // 수학 함수를 사용하기 위한 헤더 파일
+#include <unordered_map> // 해시맵(unordered_map)을 사용하기 위한 헤더 파일
+#include <map>          // 맵(map, 정렬된 딕셔너리)을 사용하기 위한 헤더 파일
+#include <sstream>      // 문자열 스트림을 사용하기 위한 헤더 파일
+#include <cstdlib>      // 유틸리티 함수(예: rand(), exit())를 위한 헤더 파일
+#include <iomanip>      // 입출력 서식을 지정하기 위한 헤더 파일
+#include <climits>      // INT_MAX(최대 정수값)을 사용하기 위한 헤더 파일
+#include <numeric>      // accumulate(누적 합)를 사용하기 위한 헤더 파일
+#include <limits.h>     // 시스템 한계값들을 정의하는 헤더 파일 (ex: LONG_MAX)
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    // 입출력 속도를 향상시키기 위한 설정
+    ios::sync_with_stdio(false);     // C와 C++ 입출력 동기화를 비활성화하여 속도 향상
+    cin.tie(NULL);  // 입력과 출력 스트림을 분리해 성능 최적화
+    cout.tie(NULL); // 출력 스트림을 비동기화하여 성능 최적화
+
+    int N, M;
+    cin >> N >> M; // 행렬의 크기 입력
+
+    vector<vector<int>> A(N, vector<int>(M)); // 행렬 A
+    vector<vector<int>> B(N, vector<int>(M)); // 행렬 B
+    vector<vector<int>> C(N, vector<int>(M)); // 결과 행렬 C
+
+    // 행렬 A 입력
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+            cin >> A[i][j];
+        }
+    }
+
+    // 행렬 B 입력
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+            cin >> B[i][j];
+        }
+    }
+
+    // 행렬 A와 B 더하기
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    // 결과 행렬 C 출력
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+            cout << C[i][j];
+            if (j < M - 1) {
+                cout << " "; // 공백 출력
+            }
+        }
+        cout << endl; // 줄바꿈
+    }
+
+    return 0;
+}
