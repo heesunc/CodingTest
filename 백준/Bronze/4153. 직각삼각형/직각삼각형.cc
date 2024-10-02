543b586e70458c062ecc2bc39169b06bc0d99b90
heesunc <112846188+heesunc@users.noreply.github.com>
2024-10-02 11:23:34 +0900

    [Bronze III] Title: 직각삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.cc
@@ -0,0 +1,67 @@
+#include <string>       // 문자열 처리를 위한 헤더 파일
+#include <iostream>     // 입출력을 위한 헤더 파일
+#include <vector>       // 동적 배열(vector)을 사용하기 위한 헤더 파일
+#include <algorithm>    // 알고리즘 함수 (예: min())를 사용하기 위한 헤더 파일
+#include <stack>        // 스택 자료구조를 사용하기 위한 헤더 파일
+#include <queue>        // 큐 및 우선순위 큐(priority_queue)를 사용하기 위한 헤더 파일
+#include <deque>        // 덱(Deque, 양방향 큐)을 사용하기 위한 헤더 파일
+#include <cctype>       // 문자 처리를 위한 헤더 파일
+#include <stdlib.h>     // 문자열을 숫자로 변환하는 함수(예: atoi())를 위한 헤더 파일
+#include <cmath>        // 수학 함수를 사용하기 위한 헤더 파일
+#include <unordered_map> // 해시맵(unordered_map)을 사용하기 위한 헤더 파일
+#include <map>          // 맵(map, 정렬된 딕셔너리)을 사용하기 위한 헤더 파일
+#include <sstream>      // 문자열 스트림을 사용하기 위한 헤더 파일
+#include <cstdlib>      // 유틸리티 함수(예: rand(), exit())를 위한 헤더 파일
+#include <iomanip>      // 입출력 서식을 지정하기 위한 헤더 파일
+#include <climits>      // INT_MAX(최대 정수값)을 사용하기 위한 헤더 파일
+#include <numeric>      // accumulate(누적 합)를 사용하기 위한 헤더 파일
+#include <limits.h>     // 시스템 한계값들을 정의하는 헤더 파일 (ex: LONG_MAX)
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    // 입출력 속도를 향상시키기 위한 설정
+    ios::sync_with_stdio(false);     // C와 C++ 입출력 동기화를 비활성화하여 속도 향상
+    cin.tie(NULL);  // 입력과 출력 스트림을 분리해 성능 최적화
+    cout.tie(NULL); // 출력 스트림을 비동기화하여 성능 최적화
+
+    int a, b, c;
+    while (true)
+    {
+        cin >> a >> b >> c;
+        if (a == 0)
+        {
+            break;
+        }
+        // 가장 큰 값이 빗변, 나머지 두 변
+        // 피타고라스 정리를 통해 side1^2 + side2^2 = 빗변^2 만족하는지 체크
+        int hypotenuse = max({ a,b,c });
+        
+        int side1, side2;
+        // side 구하기
+        if (hypotenuse == a)
+        {
+            side1 = b; side2 = c;
+        }
+        else if (hypotenuse == b)
+        {
+            side1 = a; side2 = c;
+        }
+        else
+        {
+            side1 = a; side2 = b;
+        }
+
+        if (pow(hypotenuse, 2) == pow(side1, 2) + pow(side2, 2))
+        {
+            cout << "right" << "\n";
+        }
+        else
+        {
+            cout << "wrong" << "\n";
+        }
+    }   
+
+    return 0;
+}
