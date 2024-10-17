d4a286cf4e5807bc6869657034f1f4f0267d871b
heesunc <112846188+heesunc@users.noreply.github.com>
2024-10-17 09:31:18 +0900

    [Bronze IV] Title: 임스의 메이플컵, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29790. 임스의 메이플컵/임스의 메이플컵.cc
@@ -0,0 +1,48 @@
+#include <string>       // 문자열 처리를 위한 헤더 파일
+#include <iostream>     // 입출력을 위한 헤더 파일
+#include <vector>       // 동적 배열(vector)을 사용하기 위한 헤더 파일
+#include <algorithm>    // 알고리즘 함수 (예: min())를 사용하기 위한 헤더 파일
+#include <stack>        // 스택 자료구조를 사용하기 위한 헤더 파일
+#include <queue>        // 큐 및 우선순위 큐(priority_queue)를 사용하기 위한 헤더 파일
+#include <deque>        // 덱(Deque, 양방향 큐)을 사용하기 위한 헤더 파일
+#include <cctype>       // 문자 처리를 위한 헤더 파일
+#include <stdlib.h>     // 문자열을 숫자로 변환하는 함수(예: atoi())를 위한 헤더 파일
+#include <cmath>        // 수학 함수를 사용하기 위한 헤더 파일
+#include <unordered_map> // 해시맵(unordered_map)을 사용하기 위한 헤더 파일
+#include <map>          // 맵(map, 정렬된 딕셔너리)을 사용하기 위한 헤더 파일
+#include <sstream>      // 문자열 스트림을 사용하기 위한 헤더 파일
+#include <cstdlib>      // 유틸리티 함수(예: rand(), exit())를 위한 헤더 파일
+#include <iomanip>      // 입출력 서식을 지정하기 위한 헤더 파일
+#include <climits>      // INT_MAX(최대 정수값)을 사용하기 위한 헤더 파일
+#include <numeric>      // accumulate(누적 합)를 사용하기 위한 헤더 파일
+#include <limits.h>     // 시스템 한계값들을 정의하는 헤더 파일 (ex: LONG_MAX)
+#include <ctime>
+#include <set>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	// 백준 해결 문제수 n
+	// 유니온 레벨 u
+	// 최고 레벨 l
+
+	int n, u, l; cin >> n >> u >> l;
+	int ncon = 1000; int ucon = 8000; int lcon = 260;
+
+	if (n >= ncon && (u >= ucon || l >= lcon))
+	{
+		cout << "Very Good" << "\n";
+	}
+	else if (n >= ncon && (u < ucon || l < lcon))
+	{
+		cout << "Good" << "\n";
+	}
+	else
+	{
+		cout << "Bad" << "\n";
+	}
+
+	return 0;
+}
