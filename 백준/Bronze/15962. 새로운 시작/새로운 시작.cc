82132b260bdafae3c5ab112a1eae13baf4225c7c
heesunc <112846188+heesunc@users.noreply.github.com>
2024-10-25 10:28:43 +0900

    [Bronze V] Title: 새로운 시작, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.cc
@@ -0,0 +1,12 @@
+#include <string>       // 문자열 처리를 위한 헤더 파일
+#include <iostream>     // 입출력을 위한 헤더 파일
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	
+	cout << "파이팅!!" << "\n";
+
+    return 0;
+}
